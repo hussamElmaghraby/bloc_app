b85d5f0e491f916c64d4f287cbff6af4706a9253
Parabeac-Nest <admin@parabeac.com>
2022-10-31 05:50:09 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/views/page_1/shot6.g.dart
@@ -0,0 +1,243 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+//
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+
+import 'package:flutter/material.dart';
+
+class Shot6 extends StatefulWidget {
+  const Shot6({
+    Key? key,
+  }) : super(key: key);
+  @override
+  _Shot6 createState() => _Shot6();
+}
+
+class _Shot6 extends State<Shot6> {
+  _Shot6();
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: Stack(children: [
+        Positioned(
+          left: 0,
+          width: 1600.0,
+          top: 0,
+          height: 1200.0,
+          child: Container(
+            height: 1200.0,
+            width: 1600.0,
+            decoration: BoxDecoration(
+              color: Color(0xffededed),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 179.0,
+          width: 375.0,
+          top: 224.0,
+          height: 812.0,
+          child: Container(
+              decoration: BoxDecoration(),
+              child: Stack(children: [
+                Positioned(
+                  left: 0,
+                  width: 375.0,
+                  top: 0,
+                  height: 812.0,
+                  child: Container(
+                    height: 812.0,
+                    width: 375.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius:
+                          BorderRadius.all(Radius.circular(47.999996185302734)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0x0c323b63),
+                          spreadRadius: 208,
+                          blurRadius: 208,
+                          offset: Offset(70, 133),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  width: 375.0,
+                  top: 0,
+                  height: 812.0,
+                  child: Image.asset(
+                    'assets/images/rectangle.png',
+                    package: 'testpro',
+                    height: 812.0,
+                    width: 375.0,
+                    fit: BoxFit.none,
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  width: 375.0,
+                  top: 0,
+                  height: 812.0,
+                  child: Image.asset(
+                    'assets/images/foodandrestaurant.png',
+                    package: 'testpro',
+                    height: 812.0,
+                    width: 375.0,
+                    fit: BoxFit.none,
+                  ),
+                ),
+              ])),
+        ),
+        Positioned(
+          left: 1045.0,
+          width: 375.0,
+          top: 244.0,
+          height: 812.0,
+          child: Container(
+              decoration: BoxDecoration(),
+              child: Stack(children: [
+                Positioned(
+                  left: 0,
+                  width: 375.0,
+                  top: 0,
+                  height: 812.0,
+                  child: Container(
+                    height: 812.0,
+                    width: 375.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius:
+                          BorderRadius.all(Radius.circular(47.999996185302734)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0x0c323b63),
+                          spreadRadius: 208,
+                          blurRadius: 208,
+                          offset: Offset(70, 133),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  width: 375.0,
+                  top: 0,
+                  height: 812.0,
+                  child: Container(
+                    height: 812.0,
+                    width: 375.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius:
+                          BorderRadius.all(Radius.circular(47.999996185302734)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0x12323b63),
+                          spreadRadius: 371,
+                          blurRadius: 371,
+                          offset: Offset(130, 133),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  width: 375.0,
+                  top: 0,
+                  height: 812.0,
+                  child: Image.asset(
+                    'assets/images/trackorder.png',
+                    package: 'testpro',
+                    height: 812.0,
+                    width: 375.0,
+                    fit: BoxFit.none,
+                  ),
+                ),
+              ])),
+        ),
+        Positioned(
+          left: 612.0,
+          width: 375.0,
+          top: 144.0,
+          height: 812.0,
+          child: Container(
+              decoration: BoxDecoration(),
+              child: Stack(children: [
+                Positioned(
+                  left: 0,
+                  width: 375.0,
+                  top: 0,
+                  height: 812.0,
+                  child: Container(
+                    height: 812.0,
+                    width: 375.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius:
+                          BorderRadius.all(Radius.circular(47.999996185302734)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0x26323b63),
+                          spreadRadius: 250,
+                          blurRadius: 250,
+                          offset: Offset(150, 180),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  width: 375.0,
+                  top: 0,
+                  height: 812.0,
+                  child: Container(
+                    height: 812.0,
+                    width: 375.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius:
+                          BorderRadius.all(Radius.circular(47.999996185302734)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0x12323b63),
+                          spreadRadius: 371,
+                          blurRadius: 371,
+                          offset: Offset(130, 133),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  width: 375.0,
+                  top: 0,
+                  height: 1094.0,
+                  child: Image.asset(
+                    'assets/images/fooddetails.png',
+                    package: 'testpro',
+                    height: 1094.0,
+                    width: 375.0,
+                    fit: BoxFit.none,
+                  ),
+                ),
+              ])),
+        ),
+      ]),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
